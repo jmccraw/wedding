a47004371d3558ba15d7b31809a129a8970cfcab
Justin McCraw <justin.mccraw@espn.com>
2020-05-28 18:53:35 -0400

    Rename some files; add a new interior component folder; add some opener.json test data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import init from './components/Opener.js';
-import Opener from './components/Opener.hbs';
-
-const openerData = {
-  openerTitle: 'Your content here!',
-  buttonCTA: 'Change Color'
-};
+import init from './components/opener/Opener.js';
 
 const App = () => {
-  const _opener = document.querySelector( '.opener' );
-  _opener.innerHTML = Opener( openerData );
   init();
 };
 
