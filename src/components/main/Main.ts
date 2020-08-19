58a28c875cba5622d554b4c0f3d22f23322efa4b
Justin McCraw <justin.mccraw@espn.com>
2020-08-19 14:25:53 -0400

    Add Main logic to track orientation change and when the tab gets lost

--- src/components/main/Main.ts
+++ src/components/main/Main.ts
@@ -2,10 +2,26 @@
  * Main TS
  */
 import { setIsMobile, getIsMobile } from '../../utilities/IsMobile.js';
+import { trackVisibilityChange, getVisibilityChange } from '../../utilities/TrackTabFocus.js';
+import { trackOrientationChange } from '../../utilities/TrackOrientation.js';
+
+/**
+ * Attach various event listeners
+ */
+function attachEventListeners() {
+  const visibilityChange = getVisibilityChange();
+
+  window.addEventListener( 'orientationchange', trackOrientationChange, { passive: true } );
+  window.addEventListener( visibilityChange, trackVisibilityChange, { passive: true } );
+}
 
 /**
  * Initialize Main
  */
 export default function init() {
   setIsMobile( 636 );
+  attachEventListeners();
+  window.setTimeout( () => {
+    trackOrientationChange( null, true );
+  }, 3500 );
 }
