a12423d2c4e649a76b418b8bd2ee6a86ffe57746
Justin McCraw <justin.mccraw@espn.com>
2020-08-19 12:53:44 -0400

    Add a Main wrapper for all the available HBS components

---
+++ src/components/main/Main.ts
@@ -0,0 +1,11 @@
+/**
+ * Main TS
+ */
+import { setIsMobile, getIsMobile } from '../../utilities/IsMobile.js';
+
+/**
+ * Initialize Main
+ */
+export default function init() {
+  setIsMobile( 636 );
+}
