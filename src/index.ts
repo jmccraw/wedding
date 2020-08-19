0847b7ac54c7dd9e3141d6e6f160bc2d4e5aeb2d
Justin McCraw <justin.mccraw@espn.com>
2020-08-19 19:47:37 -0400

    Add Week TS file to main index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import initMain from './components/main/Main';
 import initOpener from './components/opener/Opener';
 import initStickyHeader from './components/sticky-header/StickyHeader';
+import initWeek from './components/week/Week';
 
 const App = () => {
   initMain();
   initOpener();
   initStickyHeader();
+  initWeek();
 };
 
 App();
