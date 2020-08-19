71c12a2aeb2212c6d9a64abcd9f53a8948a5e55c
Justin McCraw <justin.mccraw@espn.com>
2020-08-19 12:54:42 -0400

    Adjust the Index to include some extra logic and stuff that we need for the page to work; including the ESPN styleguide CSS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import init from './components/opener/Opener';
+import initMain from './components/main/Main';
+import initOpener from './components/opener/Opener';
+import initStickyHeader from './components/sticky-header/StickyHeader';
 
 const App = () => {
-  init();
+  initMain();
+  initOpener();
+  initStickyHeader();
 };
 
 App();
