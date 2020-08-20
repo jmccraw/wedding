7a4094aa25f04750c2a86410c597558701551c04
Justin McCraw <justin.mccraw@espn.com>
2020-08-20 17:39:35 -0400

    Add the Performers section

---
+++ src/components/week/performers/Performers.ts
@@ -0,0 +1,11 @@
+/**
+ * Performers section animations JS
+ */
+import trackAnalyticsEvent from '../../../utilities/TrackAnalytics.js';
+
+/**
+ * Initiate the Performers
+ */
+export default function init() {
+
+}
