3ee6db048b70d7e7c21ec27b75b39372344366de
Justin McCraw <justin.mccraw@espn.com>
2020-08-20 17:41:39 -0400

    Run the Image Helper for image lazy loading

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import initOpener from './components/opener/Opener';
 import initStickyHeader from './components/sticky-header/StickyHeader';
 import initWeek from './components/week/Week';
 
+import initImageHelper from './components/image-helper/ImageHelper';
+
 const App = () => {
   initMain();
   initOpener();
   initStickyHeader();
   initWeek();
+
+  initImageHelper();
 };
 
 App();
