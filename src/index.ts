efa4d25057ee1029750f1bc7c225937a1baa3d74
Justin McCraw <justin.mccraw@espn.com>
2020-08-24 19:00:51 -0400

    Initialize Related Stories logic in the index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import initMain from './components/main/Main';
 import initOpener from './components/opener/Opener';
 import initStickyHeader from './components/sticky-header/StickyHeader';
 import initWeek from './components/week/Week';
-
+import initRelatedStories from './components/related-stories/RelatedStories';
 import initImageHelper from './components/image-helper/ImageHelper';
 
 const App = () => {
@@ -13,7 +13,7 @@ const App = () => {
   initOpener();
   initStickyHeader();
   initWeek();
-
+  initRelatedStories();
   initImageHelper();
 };
 
