1d487fee83deab2064cb39a5a2e9a96687af909e
Justin McCraw <justin.mccraw@espn.com>
2020-08-27 18:02:15 -0400

    Add new Week Opener logic to main Week TS

--- src/components/week/Week.ts
+++ src/components/week/Week.ts
@@ -2,6 +2,7 @@
  * Weeks TS
  * Injests all of the subcomponent JS here
  */
+import initOpener from './opener/Opener';
 import initStickyBits from './sticky-bits/StickyBits';
 import initPerformers from './performers/Performers';
 import initTable from './table/Table';
@@ -10,6 +11,7 @@ import initTable from './table/Table';
  * Initialize the Week overarching container and all its possible components
  */
 export default function init() {
+  initOpener();
   initStickyBits();
   initPerformers();
   initTable();
