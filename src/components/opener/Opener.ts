d96b2bdc827a0436698ed72494ef3f521a7016aa
Justin McCraw <jmccraw1@gmail.com>
2023-03-05 11:46:08 -0500

    feat: update Opener logic to not do the opacity transition at the end and to only use gsap for animations; add gradient

--- src/components/opener/Opener.ts
+++ src/components/opener/Opener.ts
@@ -8,6 +8,73 @@
  * {@link https://codepen.io/creativeocean/pen/qBbBLyB}
  */
 function initiateOpenerSequence() {
+  window.gsap.timeline({ scrollTrigger:
+      {
+        end: 'bottom bottom',
+        scrub: 0.5,
+        start: 'top top',
+        trigger: '.opener__scroller',
+      }
+    })
+    .fromTo('.opener__background', { y: 0 }, { y: -200 }, 0)
+    .fromTo('.opener__fixed', { y: -10 }, { y: -800 }, 0)
+    .fromTo('.opener__garden', { y: -50 }, { y: -650 }, 0)
+    .fromTo('.opener__center', { y: -10 }, { y: -150 }, 0)
+    .fromTo('.opener__justin', { y: -20, x: -10 }, { y: -500, x: 0 }, 0)
+    .fromTo('.opener__tyler', { y: -50, x: 10 }, { y: -550, x: 0}, 0)
+    .fromTo('.opener__accent-line', { opacity: 0, y: -50 }, { delay: 0.2, opacity: 1, y: 0 }, 0);
+
+  window.setTimeout(() => {
+    window.gsap.to('.opener__container', { opacity: 1, duration: 2 });
+  }, 500);
+
+  window.setTimeout(() => {
+    window.gsap.to('.opener__justin', { opacity: 1, duration: 0.5 });
+  }, 1000);
+
+  window.setTimeout(() => {
+    window.gsap.to('.opener__tyler', { opacity: 1, duration: 0.5 });
+  }, 1500);
+
+  window.setTimeout(() => {
+    window.gsap.to('.opener__names', { opacity: 1, duration: 1 });
+    window.gsap.to('.opener__arrow', { opacity: 1, y: 0, duration: 0.5, delay: 0.5 });
+    window.ScrollTrigger.refresh();
+  }, 2000);
+}
+
+function enterLoadingState() {
+  const _images: NodeList = document.querySelectorAll('.image-load-check');
+  let imagesToLoad = _images.length;
+
+  _images.forEach((_image: SVGImageElement) => {
+    const _newImage = document.createElement('img');
+
+    _newImage.onload = () => {
+      _newImage.onload = null;
+      imagesToLoad--;
+    };
+
+    _newImage.setAttribute('src', _image.getAttribute('xlink:href'));
+  });
+
+  const checkInterval = window.setInterval(() => {
+    if (0 === imagesToLoad) {
+      clearIntervalAndInitiateOpener();
+    }
+  }, 100);
+
+  const clearIntervalAndInitiateOpener = () => {
+    window.clearInterval(checkInterval);
+    initiateOpenerSequence();
+  };
+
+  window.setTimeout(() => {
+    if (0 < imagesToLoad) clearIntervalAndInitiateOpener();
+  }, 3000);
+}
+
+function setInitialGsapStates() {
   window.gsap.set('.opener__container', {
     height: '100%',
     left: '50%',
@@ -19,58 +86,22 @@ function initiateOpenerSequence() {
     z: '0',
   });
 
+  window.gsap.set(['.opener__justin', '.opener__tyler', '.opener__names'], {
+    opacity: 0,
+  });
+
+  window.gsap.set('.opener__arrow', {
+    opacity: 0,
+    y: -10,
+  });
+
   window.gsap.set('.opener__scroller', {
     height: '170vh',
     width: '100%',
   });
-
-  window.gsap.timeline({ scrollTrigger:
-      {
-        end: 'bottom bottom',
-        scrub: 0.5,
-        start: 'top top',
-        trigger: '.opener__scroller',
-      }
-    })
-    .fromTo('.opener__background', { y: 0 }, { y: -200 }, 0)
-    .fromTo('.opener__fixed', { y: -10 }, { y: -800 }, 0)
-    // .fromTo('.opener__honeysuckle', { y: -100 }, { y: -500 }, 0)
-    .fromTo('.opener__garden', { y: -50 }, { y: -650 }, 0)
-    .fromTo('.opener__center', { y: -10 }, { y: -150 }, 0)
-    .fromTo('.opener__justin', { y: -20, x: -10 }, { y: -500, x: 0 }, 0)
-    .fromTo('.opener__tyler', { y: -50, x: 10 }, { y: -550, x: 0}, 0)
-    .fromTo('.opener__accent-line', { opacity: 0, y: -50 }, { delay: 0.2, opacity: 1, y: 0 }, 0);
-  const timeline = window.gsap.timeline({
-      scrollTrigger: {
-        end: 'bottom +=10vh',
-        scrub: 0.5,
-        start: 'bottom bottom',
-        trigger: '.opener__scroller',
-      }
-    })
-    .fromTo('.opener__container', { opacity: 1 }, { opacity: 0 }, 0);
-
-  window.setTimeout(() => {
-    if (window.scrollY < window.innerHeight * 1.7) {
-      timeline.to('.opener__container', { opacity: 1, duration: 2 });
-    }
-
-    timeline.scrollTrigger.refresh();
-  }, 500);
-
-  window.setTimeout(() => {
-    document.querySelector('.opener__justin').classList.remove('is-hidden');
-  }, 1000);
-
-  window.setTimeout(() => {
-    document.querySelector('.opener__tyler').classList.remove('is-hidden');
-  }, 1500);
-
-  window.setTimeout(() => {
-    document.querySelector('.opener__names').classList.remove('is-hidden');
-  }, 2000);
 }
 
 export default function init() {
-  initiateOpenerSequence();
+  setInitialGsapStates();
+  enterLoadingState();
 }
